c12ba019984477d131dbac94f3d0b78d35bcce84
YEYI <361936738@qq.com>
2024-05-21 19:23:59 +0800

    feat: 输入框、多行输入

--- packages/components/docs/components/Input/demos/json-schema.tsx
+++ packages/components/docs/components/Input/demos/json-schema.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 // @ts-ignore
-import { Input, FormLayout, FormItem, FormButtonGroup, Submit } from '@formily/antd-mobile'
+import {
+  Input,
+  FormLayout,
+  FormItem,
+  FormButtonGroup,
+  Submit,
+} from '@formily/antd-mobile'
 import { Dialog } from 'antd-mobile'
 import { createForm } from '@formily/core'
-import { FormProvider, createSchemaField, } from '@formily/react'
-
+import { FormProvider, createSchemaField } from '@formily/react'
 
 const SchemaField = createSchemaField({
   components: {
@@ -22,41 +27,42 @@ const schema = {
       title: '姓名',
       'x-decorator': 'FormItem',
       'x-decorator-props': {
-        feedbackLayout: 'popover',
         tooltip: '请输入真实姓名',
       },
       'x-component': 'Input',
       'x-component-props': {
-        placeholder: '请输入姓名'
+        placeholder: '请输入姓名',
+        allowClear: true,
+        maxLength: 2,
       },
-      'x-validator': [
-        {required: true, message: '姓名不能为空'}
-      ]
+      'x-validator': [{ required: true, message: '姓名不能为空' }],
     },
     address: {
       type: 'string',
       title: '地址',
       'x-decorator': 'FormItem',
-      'x-component': 'Input',
+      'x-component': 'Input.TextArea',
       'x-component-props': {
-        placeholder: '请输入地址'
+        placeholder: '请输入',
+        maxLength: 1000,
+        showCount: true,
+        autoSize: true,
       },
     },
   },
 }
 
 export default () => {
-
   const onSubmit = (values: any) => {
     Dialog.alert({
-      content: JSON.stringify(values)
+      content: JSON.stringify(values),
     })
   }
 
   return (
     <FormProvider form={form}>
       <FormLayout>
-        <SchemaField schema={schema}/>
+        <SchemaField schema={schema} />
       </FormLayout>
       <FormButtonGroup>
         <Submit onSubmit={onSubmit}>提交</Submit>
--- packages/components/src/input/index.tsx
+++ packages/components/src/input/index.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: YEYI 361936738@qq.com
+ * @Date: 2024-05-21 17:18:09
+ * @LastEditors: YEYI 361936738@qq.com
+ * @LastEditTime: 2024-05-21 19:14:50
+ * @FilePath: /antd-mobile/packages/components/src/input/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react'
 import { connect, mapProps, mapReadPretty } from '@formily/react'
 import { Input as AntdInput, TextArea as AntdTextArea } from 'antd-mobile'
@@ -12,9 +20,10 @@ type ComposedInput = React.FC<InputProps> & {
 
 export const Input: ComposedInput = connect(
   AntdInput,
-  mapProps((props) => {
+  mapProps((props: any) => {
     return {
       value: isValid(props.value) ? props.value : '',
+      clearable: props.allowClear,
     }
   }),
   mapReadPretty(PreviewText.Input)
