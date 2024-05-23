873ce08ea353daf45428a737feeecf33e24806f4
YEYI <361936738@qq.com>
2024-05-23 10:01:24 +0800

    feat: 修改卡片的展示内容

--- packages/components/docs/components/Card/demos/json-schema.tsx
+++ packages/components/docs/components/Card/demos/json-schema.tsx
@@ -2,12 +2,11 @@
  * @Author: YEYI 361936738@qq.com
  * @Date: 2024-05-21 18:59:51
  * @LastEditors: YEYI 361936738@qq.com
- * @LastEditTime: 2024-05-22 15:36:04
+ * @LastEditTime: 2024-05-22 17:51:04
  * @FilePath: /antd-mobile/packages/components/docs/components/Radio/demos/json-schema.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from 'react'
-// @ts-ignore
 import {
   FormItem,
   FormButtonGroup,
@@ -15,6 +14,11 @@ import {
   Radio,
   Card,
   Form,
+  Input,
+  Checkbox,
+  FormGrid,
+  Switch,
+  // @ts-ignore
 } from '@formily/antd-mobile'
 import { Dialog } from 'antd-mobile'
 import { createForm } from '@formily/core'
@@ -25,6 +29,10 @@ const SchemaField = createSchemaField({
     FormItem,
     Radio,
     Card,
+    Input,
+    Checkbox,
+    FormGrid,
+    Switch,
   },
 })
 
@@ -51,45 +59,179 @@ const form = createForm()
 const schema = {
   type: 'object',
   properties: {
-    lkrsql60n3ap: {
-      type: 'void',
+    h8mhtis24ak1: {
       'x-component': 'Card',
+      'x-designable-id': 'h8mhtis24ak1',
       'x-component-props': {
-        title: 'JSON Schema 模式',
-        extra: '右边扩展',
+        extra: '111',
+        title: '基本信息',
       },
-      'x-designable-id': 'lkrsql60n3ap',
+      type: 'void',
       'x-index': 0,
       properties: {
-        name: {
-          type: 'Array<string | number>',
-          title: '单选框',
-          enum: ItemList,
-          'x-decorator': 'FormItem',
-          'x-component': 'Radio.Group',
-          'x-decorator-props': {
-            feedbackLayout: 'popover',
-            tooltip: '额外提示',
-          },
+        a0mn32qv1442: {
+          'x-component': 'FormGrid',
+          'x-designable-id': 'a0mn32qv1442',
           'x-component-props': {
-            placeholder: '请选择',
+            columnGap: 24,
           },
-          'x-validator': [{ required: true, message: '必须选择一项' }],
+          type: 'void',
+          'x-index': 0,
+          properties: {
+            tvvq1pgj3pju: {
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'tvvq1pgj3pju',
+              'x-component-props': {},
+              type: 'void',
+              'x-index': 0,
+              'x-validator': [],
+              properties: {
+                seahhplccrp2: {
+                  type: 'string',
+                  title: '输入框',
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Input',
+                  'x-component-props': {
+                    placeholder: '请输入',
+                  },
+                  'x-validator': [],
+                  'x-decorator-props': {},
+                  'x-designable-id': 'seahhplccrp2',
+                  'x-index': 0,
+                },
+              },
+            },
+            g8jxwy2bpj9f: {
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'g8jxwy2bpj9f',
+              type: 'void',
+              'x-index': 1,
+              properties: {
+                wscmug9326q9: {
+                  default: false,
+                  'x-component': 'Switch',
+                  'x-decorator': 'FormItem',
+                  'x-designable-id': 'wscmug9326q9',
+                  'x-decorator-props': {},
+                  'x-validator': [],
+                  'x-component-props': {},
+                  type: 'boolean',
+                  title: '开关2',
+                  'x-index': 0,
+                },
+              },
+            },
+            d22mwrrz7rra: {
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'd22mwrrz7rra',
+              type: 'void',
+              'x-index': 2,
+              properties: {
+                bjg7dhguuycr: {
+                  type: 'string',
+                  title: '多行输入',
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Input.TextArea',
+                  'x-component-props': {
+                    placeholder: '请输入',
+                    maxLength: 1000,
+                    showCount: true,
+                  },
+                  'x-validator': [],
+                  'x-decorator-props': {},
+                  'x-designable-id': 'bjg7dhguuycr',
+                  'x-index': 0,
+                },
+              },
+            },
+            jcsf6savxrgv: {
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'jcsf6savxrgv',
+              type: 'void',
+              'x-index': 3,
+            },
+          },
         },
-        name2: {
-          type: 'Array<string | number>',
-          title: '单选框',
-          enum: ItemList,
-          'x-decorator': 'FormItem',
-          'x-component': 'Radio.Group',
-          'x-decorator-props': {
-            feedbackLayout: 'popover',
-            tooltip: '额外提示',
-          },
+        dhumdwcqvrnb: {
+          type: 'void',
+          'x-component': 'FormGrid',
           'x-component-props': {
-            placeholder: '请选择',
+            columnGap: 24,
           },
-          'x-validator': [{ required: true, message: '必须选择一项' }],
+          'x-validator': [],
+          'x-designable-id': 'dhumdwcqvrnb',
+          properties: {
+            wdvh1y943bxj: {
+              type: 'void',
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'wdvh1y943bxj',
+              properties: {
+                yvmj50rk2ehc: {
+                  type: 'Array<string | number>',
+                  title: '复选框组',
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Checkbox.Group',
+                  enum: [
+                    {
+                      label: '选项1',
+                      value: 1,
+                    },
+                    {
+                      label: '选项2',
+                      value: 2,
+                    },
+                  ],
+                  'x-validator': [],
+                  'x-decorator-props': {},
+                  'x-designable-id': 'yvmj50rk2ehc',
+                  'x-index': 0,
+                },
+              },
+              'x-index': 0,
+            },
+            bia9w88ltljo: {
+              type: 'void',
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'bia9w88ltljo',
+              properties: {
+                s2ayqfdzzu8b: {
+                  type: 'string | number',
+                  title: '单选框组',
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Radio.Group',
+                  enum: [
+                    {
+                      label: '选项1',
+                      value: 1,
+                    },
+                    {
+                      label: '选项2',
+                      value: 2,
+                    },
+                  ],
+                  'x-validator': [],
+                  'x-component-props': {},
+                  'x-decorator-props': {},
+                  'x-designable-id': 's2ayqfdzzu8b',
+                  'x-index': 0,
+                },
+              },
+              'x-index': 1,
+            },
+            ktaau5i6jvrs: {
+              type: 'void',
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'ktaau5i6jvrs',
+              'x-index': 2,
+            },
+            j2gt3jsuorcp: {
+              type: 'void',
+              'x-component': 'FormGrid.GridColumn',
+              'x-designable-id': 'j2gt3jsuorcp',
+              'x-index': 3,
+            },
+          },
+          'x-index': 1,
         },
       },
     },
